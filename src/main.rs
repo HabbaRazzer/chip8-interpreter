abeb6aa9c23e058bf93abd506388aaeafdba588e
Nick Martinez <habbarazzer@gmail.com>
2017-10-09 22:12:56 -0400

    Starting out simple...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct System {
+    memory: [u8; 4096],
+    registers: [u8; 16],
+    pc: u16,
+    index: u16
+}
+
+impl System {
+    fn new() -> Self {
+        System {
+            memory: [0; 4096],
+            registers: [0; 16],
+            pc: 0,
+            index: 0
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let system = System::new();
+    println!("{}, {}, {}, {}", system.memory[0], system.registers[0],
+        system.pc, system.index);
 }
