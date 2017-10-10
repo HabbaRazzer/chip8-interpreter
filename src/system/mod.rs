5964b4c696b21b920f2109b7105418907aed1f0e
Nick Martinez <habbarazzer@gmail.com>
2017-10-10 11:43:53 -0400

    read word from system memory

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -11,7 +11,7 @@ pub struct System {
     sp: u8
 }
 
-impl System {
+impl System{
     pub fn new() -> Self {
         System {
             memory: [0; NUM_BYTES],
@@ -21,5 +21,34 @@ impl System {
             stack: [0; STACK_SIZE],
             sp: 0
         }
+    }
+
+    pub fn read_word(&self, address: usize) -> &[u8] {
+        &self.memory[address..address + 2]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /** Load some test data into the system memory for the purposes of testing. */
+    fn load_test_data(system: &mut System) {
+        system.memory[0] = 0xAF;
+        system.memory[1] = 0x7F;
+        system.memory[NUM_BYTES / 2] = 0x77;
+        system.memory[NUM_BYTES / 2 + 1] = 0x88;
+        system.memory[NUM_BYTES - 2] = 0x3A;
+        system.memory[NUM_BYTES - 1] = 0x01;
+    }
+
+    /** Read a word (two bytes) from the system memory. */
+    #[test]
+    fn read_word() {
+        let mut system = System::new();
+        load_test_data(&mut system);
+        assert_eq!([0xAF, 0x7F], system.read_word(0));
+        assert_eq!([0x77, 0x88], system.read_word(NUM_BYTES / 2));
+        assert_eq!([0x3A, 0x01], system.read_word(NUM_BYTES - 2));
     }
 }
